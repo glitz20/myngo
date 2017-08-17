b33256b81da93379c023ede0df0aba3ddedaedb3
sabin61011 <ssapkota20@my.whitworth.edu>
2017-08-16 23:09:00 -0700

    Aa

---
+++ controllers/image.js
@@ -0,0 +1,94 @@
+var fs = require('fs'),
+    path = require('path'),
+    sidebar = require('../helpers/sidebar'),
+    Models = require('../models');
+
+module.exports = {
+    index: function(req, res){
+        var viewModel = {
+            image:{},
+            comments: []
+        };
+        
+        Models.Image.findOne({filename: {$regex: req.params.image_id}},
+        function(err, image){
+            if (err){throw err;}
+            if(image){
+                image.views = image.views + 1;
+                viewModel.image = image;
+                image.save();
+                Models.Comment.find({image_id: image._id}, {}, {sort: {'timestamp':1}},
+                                   
+                function(err,comments){
+                    if(err) {throw err;}
+                    
+                    viewModel.comments=comments;
+                    sidebar(viewModel, function(viewModel){
+                        res.render('image', viewModel);
+                    });
+                }                   
+            );
+            }
+            else{
+                res.redirect('/');
+            }
+        });
+        sidebar(viewModel,function(viewModel){
+             res.render('image', viewModel);
+        });
+       
+    },
+    
+    create: function(req, res){
+        var saveImage = function(){
+          var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
+              imgUrl = '';
+            
+            for(var i=0; i<6; i+=1){
+                imgUrl +=possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            
+            Models.Image.find({filename: imgUrl}, function(err, images){
+                if(images.length>0){
+                    saveImage();
+                }
+                else{
+                var tempPath = req.files.file.path,
+                ext = path.extname(req.files.file.name).toLowerCase(),
+                targetPath =path.resolve('./public/upload/' + imgUrl + ext);
+            
+            if( ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext ==='.gif'){
+                fs.rename(tempPath, targetPath, function(err){
+                    if (err) {throw err;}
+                    
+                    var newImg = new Models.Image({
+                        title: req.body.title,
+                        filename:imgUrl + ext,
+                        description: req.body.description
+                    });
+                    newImg.save(function(err, image){
+                        res.redirect('/images/' + image.uniqueId);
+                    });
+                   
+                });
+            }
+            else{
+                fs.unlink(tempPath, function(){
+                    if (err) {throw err;}
+                    
+                    res.json(500, {error: 'Only image files are allowed.'});
+                });
+            }
+               }
+            });
+        };
+        saveImage();
+    },
+    
+    like: function(req, res){
+        res.json({likes: 1});
+    },
+    comment: function(req,res){
+        res.send('The image:comment POST controller');
+    }
+};
